905204b55dcd215ecf6e6d975ee544f4f7337ae8
gauravputale1416 <gauravputale2003@gmail.com>
2025-10-24 20:15:53 +0530

    added the src

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './Home.css'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 import NewTodo from '../NewTodo/NewTodo'
 
 
